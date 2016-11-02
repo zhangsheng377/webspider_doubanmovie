6180cd098c5f3e30607517489f40a6571d71b8bf
张胜东 <435878393@qq.com>
2016-11-02 14:47:15 +0800

    用api所需要的间隔比直接抓取网页要大得多，现在弄了判断出错原因的全自动爬虫
    在考虑要不要继续

--- test2.py
+++ test2.py
@@ -1,45 +1,54 @@
 # coding:utf-8
 import urllib2
-from HTMLParser import HTMLParser
-import sys
 import time
 
-reload(sys)
-sys.setdefaultencoding('utf8')
 
 
-def getHtml(url):
+def get_html(url):
     try:
         user_agent = 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.81 Safari/537.36'
         headers = {'User-Agent': user_agent}
         request = urllib2.Request(url, headers=headers)
         page = urllib2.urlopen(request)
-        html = page.read()
-        return html
+        my_html = page.read()
+        return my_html
     except urllib2.URLError, e:
         if hasattr(e, "reason"):
             print u"豆瓣链接错误，错误原因", e.reason
             print url
+            if e.reason=="Bad Request":
+                print "wait for reget_html......"
+                waitsecond_file=open("waitsecond.dat", 'r')
+                waitsecond=int(waitsecond_file.read())
+                waitsecond_file.close()
+                waitsecond_file = open("waitsecond.dat", 'w')
+                waitsecond_file.write(str(waitsecond+1))
+                waitsecond_file.close()
+                time.sleep(60*60*2)
+                return get_html(url)
         return ""
 
 
-startid_file=open("startid.dat",'r')
-start_s=startid_file.read()
-startid=int(start_s)
-startid_file.close()
-for i in range(startid+1,9999999):
-    id = str(i)
+startId_file=open("startid.dat", 'r')
+start_s=startId_file.read()
+startId=int(start_s)
+startId_file.close()
+for i in range(startId+1,9999999):
+    myId = str(i)
 
-    myurl = 'https://api.douban.com/v2/movie/subject/'+id
-    html = getHtml(myurl)
+    myUrl = 'https://api.douban.com/v2/movie/subject/' + myId
+    html = get_html(myUrl)
     print i, "\t", len(html)
     if len(html) > 200:
-        file = open("output.dat", 'a+')
-        file.write(html+"\n")
-        file.close()
-    startid_file = open("startid.dat", 'w')
-    startid_file.write(str(i))
-    startid_file.close()
-    time.sleep(6)
+        my_file = open("output.dat", 'a+')
+        my_file.write(html + "\n")
+        my_file.close()
+    startId_file = open("startid.dat", 'w')
+    startId_file.write(str(i))
+    startId_file.close()
+    waitsecond_file=open("waitsecond.dat", 'r')
+    waitsecond=int(waitsecond_file.read())
+    waitsecond_file.close()
+    time.sleep(waitsecond)
 
 
